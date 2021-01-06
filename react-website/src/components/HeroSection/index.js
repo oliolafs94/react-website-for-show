88d03d4948392ccf355c15ce0061b80c2d751e55
Óli Ólafsson <oliolafs@Olis-MacBook-Pro-3.local>
2021-01-06 21:31:27 +0000

    Navbar done, HeroSecrctiion done, infosections done, and more little changes were made

---
+++ react-website/src/components/HeroSection/index.js
@@ -0,0 +1,33 @@
+import React, { useState }from 'react';
+import Video from '../../videos/video.mp4';
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements';
+import { Button } from '../ButtonElements';
+
+const HeroSection = () => {
+
+    const [hover, setHover] =  useState(false);
+
+    const onHover = () => {
+        setHover(!hover);
+    }
+
+    return (
+        <HeroContainer>
+            <HeroBg>
+                <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+            </HeroBg>
+            <HeroContent>
+                <HeroH1>My React Website</HeroH1>
+                <HeroP>by Óli Ólafsson</HeroP>
+                <HeroBtnWrapper>
+                    <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}
+                    primary="true"
+                    dark="true"
+                    >Get started {hover ? <ArrowForward /> : <ArrowRight />}</Button>
+                </HeroBtnWrapper>
+            </HeroContent>
+        </HeroContainer>
+    )
+}
+
+export default HeroSection
