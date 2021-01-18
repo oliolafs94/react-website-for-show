189101cad68dc10a02a19cab2fde3819e2c3769a
Óli Ólafsson <oliolafs@194-144-63-246.static.metronet.is>
2021-01-18 13:34:12 +0000

    fixes made to filter search cats

--- react-website/src/components/SearchComponent/cat-breeds.js
+++ react-website/src/components/SearchComponent/cat-breeds.js
@@ -2,44 +2,50 @@
 const CatBreeds = [
     {
         "id": "1",
-        "name": "Abyssinian Cat",
+        "name": "abyssinian Cat",
         "image": "https://www.purina.com/sites/g/files/auxxlc196/files/Abyssinian_body_7.jpg",
     },
     {
         "id": "2",
-        "name": "Abyssinian Cat",
-        "image": "https://www.purina.com/sites/g/files/auxxlc196/files/Abyssinian_body_7.jpg",
+        "name": "exotic Shorthair Cat Breed",
+        "image": "https://www.purina.com/sites/g/files/auxxlc196/files/styles/kraken_generic_max_width_480/public/Exotic_body_6.jpg?itok=uhjqXteg",
     },
     {
         "id": "3",
-        "name": "Abyssinian Cat",
-        "image": "https://www.purina.com/sites/g/files/auxxlc196/files/Abyssinian_body_7.jpg",
+        "name": "himalayan Cat Breed",
+        "image": "https://www.purina.com/sites/g/files/auxxlc196/files/styles/kraken_generic_max_width_480/public/Himilayan_body_6.jpg?itok=N6e_4Z73",
     },
     {
         "id": "4",
-        "name": "American Bobtail Cat Breed",
-        "image": "https://www.purina.com/sites/g/files/auxxlc196/files/Abyssinian_body_7.jpg",
+        "name": "american Bobtail Cat Breed",
+        "image": "https://www.purina.com/sites/g/files/auxxlc196/files/styles/kraken_generic_max_width_360/public/AmericanBobtail_body_6.jpg?itok=nXSRswr-",
     },
     {
         "id": "5",
-        "name": "Abyssinian Cat",
-        "image": "https://www.purina.com/sites/g/files/auxxlc196/files/Abyssinian_body_7.jpg",
+        "name": "maine Coon Cat Breed",
+        "image": "https://www.purina.com/sites/g/files/auxxlc196/files/styles/kraken_generic_max_width_480/public/MaineCoon_body_7.jpg?itok=OzKgiq0e",
     },
     {
         "id": "6",
-        "name": "Abyssinian Cat",
-        "image": "https://www.purina.com/sites/g/files/auxxlc196/files/Abyssinian_body_7.jpg",
+        "name": "munchkin Cat",
+        "image": "https://www.purina.com/sites/g/files/auxxlc196/files/styles/kraken_generic_max_width_480/public/Munchkin.jpg?itok=cITMOhMp",
     },
     {
         "id": "7",
-        "name": "Abyssinian Cat",
-        "image": "https://www.purina.com/sites/g/files/auxxlc196/files/Abyssinian_body_7.jpg",
+        "name": "ragdoll Cat Breed",
+        "image": "https://www.purina.com/sites/g/files/auxxlc196/files/styles/kraken_generic_max_width_480/public/Ragdoll_240x240%20%281%29.jpg?itok=zfZQ7u3Z",
     },
     {
         "id": "8",
-        "name": "Abyssinian Cat",
-        "image": "https://www.purina.com/sites/g/files/auxxlc196/files/Abyssinian_body_7.jpg",
+        "name": "siamese Cat Breed",
+        "image": "https://www.purina.com/sites/g/files/auxxlc196/files/styles/kraken_generic_max_width_480/public/Siamese_body_7.jpg?itok=hm8RX1zI",
     },
+    {
+        "id": "9",
+        "name": "toyger Cat Breed",
+        "image": "https://www.purina.com/sites/g/files/auxxlc196/files/styles/kraken_generic_max_width_480/public/Toyger_body_7.jpg?itok=vENnTMP2",
+    },
+
 
 ];
 
--- react-website/src/components/SearchComponent/index.js
+++ react-website/src/components/SearchComponent/index.js
@@ -1,32 +1,97 @@
+// //https://api.thecatapi.com/v1/images/search?breed_ids={breed-id}
+
+
 import CatBreed from './cat-breeds';
 import './search-style.css';
 
-import React from 'react'
+import React, {Component} from 'react'
 
-const SearchList = () => {
-    return (
-        <>
-        <h1>Cat Breeds!</h1>
-        <div>
-            <input className="input" type="text" placeholder="Type here for cat breeds in the list"/>
+// class SearchList extends Component {
+
+//     render() {
+//         return (
+//             <>
+//             <h1>Cat Breeds!</h1>
+//             <div>
+//                 <input className="input" type="text" placeholder="Type here for cat breeds in the list" onChange={this._onKeyUp}/>
+//             </div>
+//             <br/>
+//             <div style={{ display: "flex", flexWrap: "wrap" }}>
+//                 {CatBreed.map((breeds, index) => {
+//                     return (
+//                         <div style={{ flex: 1, flexBasis: "25%"}}>
+//                             <div className="polaroid">
+//                                 <img className="image" src={breeds.image} alt="kisi" />
+//                                 <div className="container">
+//                                     <h1 key={index} className="text">{breeds.name}</h1>
+//                                 </div>
+//                             </div>
+//                         </div>
+//                     )
+//                 })}
+//             </div>
+//             </>
+//         )
+//     }
+// }
+
+// export default SearchList
+
+import { Button, Input, Footer, Card, CardBody, CardImage, CardTitle, CardText } from 'mdbreact';
+
+
+class SearchList extends Component {
+
+    state = {
+        search : ""
+    }
+
+    renderCats = cat =>{
+
+        return <div style={{ marginTop : '20px', display: 'inline-block'}}>
+            <Card>
+                <CardBody>
+                    <div><img src={cat.image} className="image" alt={cat.name} /></div>
+                    <h1 title={cat.name}>{cat.name.substring(0, 30).toUpperCase()}{ cat.name.length > 30 }</h1>
+                </CardBody>
+            </Card>
         </div>
-        <br/>
-        <div style={{ display: "flex", flexWrap: "wrap" }}>
-            {CatBreed.map((breeds, index) => {
-                return (
-                    <div style={{ flex: 1, flexBasis: "25%"}}>
-                        <div className="polaroid">
-                            <img className="image" src={breeds.image} alt="kisi" />
-                            <div className="container">
-                                <h1 key={index} className="text">{breeds.name}</h1>
-                            </div>
+    }
+
+    onchange = e =>{
+        this.setState({ search : e.target.value });
+    }
+
+    render() {
+
+        const { search } = this.state;
+        const filteredCats = CatBreed.filter( cat =>{
+            return cat.name.indexOf( search ) !== -1
+        })
+
+        return (
+            <div className="img-wrap">
+            <main style={{marginTop: '4rem'}}>
+                <div className="container">
+                    <div className="row">
+                        <div className="col"></div>
+                        <div className="col">
+                            <Input className="input" icon="search" onChange={this.onchange} hint="Filter out cat breeds here"/>
                         </div>
+                        <div className="col"></div>
                     </div>
-                )
-            })}
-        </div>
-        </>
-    )
+                    <div style={{flex: 1, flexBasis: "25%"}}>
+                        {
+                            filteredCats.map( cat =>{
+                                return this.renderCats(cat)
+                            })
+                        }
+                    </div>
+                </div>
+            </main>
+            </div>
+        );
+    }
 }
 
-export default SearchList
+export default SearchList;
